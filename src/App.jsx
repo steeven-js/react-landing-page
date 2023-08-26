2922c9f5234da874dfd280da7f8ec544cded6e24
steeven-js <jacques.steeven@gmail.com>
2023-08-25 20:46:42 -0400

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer } from "./sections";
+import Nav from "./components/Nav";
 
 const App = () => (
   <main className='relative'>
